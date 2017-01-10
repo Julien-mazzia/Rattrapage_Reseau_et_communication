ac08046fb3b339b699145bd7247b6c8054e9e668
Julien-mazzia <julien.mazzia@viacesi.fr>
2017-01-10 13:05:30 +0100

    Script Powershell

---
+++ Archivage_de_fichier.ps1
@@ -0,0 +1,23 @@
+﻿
+param( 
+	[String]$repertoire,
+	#[String]$recursif,
+	[String]$extension,
+	[String]$cheminArchive
+	
+)
+
+$stockage = "c:\stockage"
+
+	If(Test-path $stockage) {Remove-item $stockage -Recurse}
+ 	New-Item -Path "c:\" -Name "stockage" -ItemType directory
+	copy-Item -Path ($repertoire+"\*"+$extension) -destination $stockage -Recurse
+	
+	If(Test-path $cheminArchive) {Remove-item $cheminArchive -Recurse}
+	[io.compression.zipfile]::CreateFromDirectory($stockage, $cheminArchive) 
+	
+	Remove-Item -path ($repertoire+"\*"+$extension) -Recurse
+	$path = $stockage+"\*"+$extension
+	$i=@(Dir $path).Count
+	Write-Host "Il y a eu $i fichiers archivés"
+	
